cf53bef1b77daf34c97c116610cc4a25ce1c88ef
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2023-01-10 21:08:20 +0100

    Added Zuul

---
+++ zuul/zuul.albert/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Zuul;
+
+namespace Zuul
+{
+    internal class Player
+    {
+        //Field
+        private int health;
+
+        //Property
+        public Room CurrentRoom { get; set; }
+        public Inventory Backpack { get; }
+        public int Health
+        {
+            get { return health; }
+        }
+
+        //Constructor
+        public Player()
+        {
+            CurrentRoom = null;
+            health = 100;
+        }
+
+        //Methods
+        public void Damage(int amount)
+        {
+            health = health - amount;
+        }
+
+        public void Heal(int amount)
+        {
+            health = health + amount;
+        }
+
+        public bool IsAlive()
+        {
+            return (health > 0);
+        }
+
+    }
+}
